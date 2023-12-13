4dadd7a9ae944d986ed2e6452fcccda9e697a7c2
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-13 03:41:01 -0500

    refactor from vanilla fetch to try/catch + async/await

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ window.addEventListener("DOMContentLoaded", () => {
     const nameInput = document.getElementById("inputBox");
     const submitButton = document.getElementById("submitButton");
     const resultDiv = document.getElementById("resultsDiv");
-    
-    function makeApiCall(name) {
+
+    async function makeApiCall(name) {
 
         url = "https://api.agify.io?name=" + name;
-
-        fetch(url).then
-        ((res) => {
-            return res.json();
-        }).then
-        ((response) => {
+        try {
+            const res = await fetch(url);
+            const response = await res.json();
             resultDiv.innerHTML = response.age;
-        })
-        .catch( (error) => {
+        } catch(error) {
             console.log(error) 
-        })
+        }
     
     };
     
@@ -30,3 +26,20 @@ window.addEventListener("DOMContentLoaded", () => {
 
 });
 
+    
+    // OLD CODE WE REFACTORED FROM INTO ASYNC/AWAIT FORMAT
+
+    // function makeApiCall(name) {
+
+    //     url = "https://api.agify.io?name=" + name;
+
+    //     fetch(url).then
+    //     ((res) => {
+    //         return res.json();
+    //     }).then
+    //     ((response) => {
+    //         resultDiv.innerHTML = response.age;
+    //     })
+    //     .catch( (error) => {
+    //         console.log(error) 
+    //     })
